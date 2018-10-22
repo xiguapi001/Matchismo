d687027031ea8a94b3e2e554b17478ad2102c968
wh <wh@whdeMacBook-Pro.local>
2018-10-22 22:56:22 +0800

    de

--- class1-2/Matchismo/Matchismo/PlayingCard.h
+++ class1-2/Matchismo/Matchismo/PlayingCard.h
@@ -13,4 +13,6 @@
 @property (nonatomic,strong)NSString* suit;//花色 ♥️ ♣️ ♠️ ♦️
 @property (nonatomic)NSUInteger rank;//点数 A 2 3 4 ... J Q K
 
++(NSArray*)vaildSuit;//类方法一般都是公共的API，因此放到这里声明
++(NSUInteger)maxRank;
 @end
--- class1-2/Matchismo/Matchismo/PlayingCard.m
+++ class1-2/Matchismo/Matchismo/PlayingCard.m
@@ -10,12 +10,36 @@
 
 @implementation PlayingCard
 
+@synthesize suit = _suit;
+
 -(NSString*) content
 {
     //return [NSString stringWithFormat:@"%lu%@",(unsigned long)self.rank,self.suit];
-    NSArray* rankStrings = @[@"?",@"A",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",
-                             @"10",@"J",@"Q",@"K"];
+    NSArray* rankStrings = [PlayingCard rankString];
     return [rankStrings[self.rank] stringByAppendingString:self.suit];
 }
-
++(NSArray*)rankString
+{
+    return @[@"?",@"A",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",
+                  @"10",@"J",@"Q",@"K"];
+}//类方法
++(NSArray*)vaildSuit
+{
+    return @[@"♥️",@"♣️",@"♠️",@"♦️"];
+}
+//这是一个类方法
+-(void)setSuit:(NSString *)suit
+{
+    if([[PlayingCard vaildSuit] containsObject:suit]){
+        _suit = suit;
+    }
+}
+-(NSString*)suit
+{
+    return _suit?_suit:@"?";
+}
++(NSUInteger)maxRank
+{
+    return [[self rankString] count] - 1;
+}
 @end
