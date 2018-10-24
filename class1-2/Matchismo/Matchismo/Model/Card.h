518649e96e8c698d25517d0d089b925b03414602
wanghui <wanghui@ykccn.com>
2018-10-24 09:26:44 +0800

    423

---
+++ class1-2/Matchismo/Matchismo/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by ykc on 2018/10/24.
+//  Copyright © 2018年 DMKJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
--- class1-2/Matchismo/Matchismo/Model/Card.m
+++ class1-2/Matchismo/Matchismo/Model/Card.m
@@ -2,14 +2,13 @@
 //  Card.m
 //  Matchismo
 //
-//  Created by wh on 2018/10/13.
+//  Created by ykc on 2018/10/24.
 //  Copyright © 2018年 DMKJ. All rights reserved.
 //
 
 #import "Card.h"
 
 @implementation Card
-
 -(int)match:(NSArray *)otherCards
 {
     int score = 0;
@@ -20,5 +19,6 @@
     }
     return score;
 }
+
 
 @end
